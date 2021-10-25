e348a717f5e323f0857e9e5752c344581ebb30d8
tajmin <tajminur.aiub@gmail.com>
2021-10-25 19:48:40 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
+
+const app = express();
+
+const port = 5000;
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7xlcz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('car-mechanic');
+        const servicesCollection = database.collection('services');
+
+        //GET: Get Services
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const result = await cursor.toArray();
+            res.send(result);
+
+        })
+
+        //GET: Get Service by Id
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await servicesCollection.findOne(query);
+            res.json(result);
+
+        })
+
+        // POST: Add Service
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            const result = await servicesCollection.insertOne(service);
+            res.send(result);
+        })
+
+        // DELETE: Delete service
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await servicesCollection.deleteOne(query);
+            res.json(result);
+        })
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    console.log('server running: ')
+    res.send('server running: ');
+})
+
+
+//opSBtIl0cY1F2gYI geniusmechanic
+
+app.listen(port, () => {
+    console.log('Port running:', port);
+})
